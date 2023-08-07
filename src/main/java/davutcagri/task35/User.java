b01def9703b6990717ab7a084b37d5b121831faa
davutcagri <71286320+davutcagri@users.noreply.github.com>
2023-08-07 13:39:06 +0300

    getters and setters

--- src/main/java/davutcagri/task35/User.java
+++ src/main/java/davutcagri/task35/User.java
@@ -1,9 +1,15 @@
 package davutcagri.task35;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
